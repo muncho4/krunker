6a8e3bb82b3bd08519a8de3b431f1a7f6cd85cac
muncho4 <win.epic202@gmail.com>
2020-10-14 19:30:45 -0400

    Updates

---
+++ precache-manifest.6974343f6313aa468f9ba3b1b65b3a8e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b97ec40f12d9d021f44f01761a40cd9",
+    "url": "/krunker/index.html"
+  },
+  {
+    "revision": "573317ed700b4ac02a68",
+    "url": "/krunker/static/css/main.28b5c244.chunk.css"
+  },
+  {
+    "revision": "b5f143b2c7ef9ead9630",
+    "url": "/krunker/static/js/2.e340177e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/krunker/static/js/2.e340177e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "573317ed700b4ac02a68",
+    "url": "/krunker/static/js/main.96268784.chunk.js"
+  },
+  {
+    "revision": "1f777c0a4635b01ba321",
+    "url": "/krunker/static/js/runtime-main.80fe70ca.js"
+  },
+  {
+    "revision": "7473b1faea0b53202d76a79dac2eae0f",
+    "url": "/krunker/static/media/AR.7473b1fa.png"
+  },
+  {
+    "revision": "f8130aa39eac0d55bd468c3e744f4180",
+    "url": "/krunker/static/media/LMG.f8130aa3.png"
+  },
+  {
+    "revision": "21ac688b8bcd90552faa0579394f636b",
+    "url": "/krunker/static/media/SMG.21ac688b.png"
+  },
+  {
+    "revision": "a7d7fcfc532bb84ac4cdd981e8041005",
+    "url": "/krunker/static/media/alienBlaster.a7d7fcfc.png"
+  },
+  {
+    "revision": "24c6618cdae9c0e185f8674017b890d4",
+    "url": "/krunker/static/media/autoPistol.24c6618c.png"
+  },
+  {
+    "revision": "73db7ed8d767f5016a87bd17ff3af001",
+    "url": "/krunker/static/media/blaster.73db7ed8.png"
+  },
+  {
+    "revision": "567bdd11669940f250767af077f89466",
+    "url": "/krunker/static/media/crossbow.567bdd11.png"
+  },
+  {
+    "revision": "eeceadacb2ba37ba4fddd58a95e68b24",
+    "url": "/krunker/static/media/deagle.eeceadac.png"
+  },
+  {
+    "revision": "b5337e73871151d3524b1038c0279c97",
+    "url": "/krunker/static/media/famas.b5337e73.png"
+  },
+  {
+    "revision": "c89266c359d55d796143521e920d955c",
+    "url": "/krunker/static/media/pistol.c89266c3.png"
+  },
+  {
+    "revision": "73d20394e19d1b1590e6e67338ddfcc4",
+    "url": "/krunker/static/media/revolver.73d20394.png"
+  },
+  {
+    "revision": "c36d07e9a8ca76849a2fda7c10da8c07",
+    "url": "/krunker/static/media/rocketLauncher.c36d07e9.png"
+  },
+  {
+    "revision": "ab0176f798bfdf2e900619fd868128b9",
+    "url": "/krunker/static/media/sawedOff.ab0176f7.png"
+  },
+  {
+    "revision": "0d999d86c49f293d33b2429c86a38a71",
+    "url": "/krunker/static/media/semiAuto.0d999d86.png"
+  },
+  {
+    "revision": "5a865cff61cd6089aa0aa7b691035890",
+    "url": "/krunker/static/media/shotgun.5a865cff.png"
+  },
+  {
+    "revision": "c6571f44c33b5a94b109675ad767a600",
+    "url": "/krunker/static/media/sniper.c6571f44.png"
+  },
+  {
+    "revision": "ac4423328c0fdb3bada8c6221ca07900",
+    "url": "/krunker/static/media/swapTime.ac442332.png"
+  },
+  {
+    "revision": "50c35a0a84def1575aa1630b61e948ad",
+    "url": "/krunker/static/media/uzi.50c35a0a.png"
+  }
+]);
